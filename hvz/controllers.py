fbd6e1cd2449728b44674042e94bba7e7565ae3e
Ross Light <ross@zombiezen.com>
2008-03-31 20:40:54 +0000

    Fixed style on controllers.py

--- hvz/controllers.py
+++ hvz/controllers.py
@@ -1,13 +1,25 @@
-import turbogears as tg
-from turbogears import controllers, expose, flash
-# from hvz import model
-from turbogears import identity, redirect
-from cherrypy import request, response
-# from hvz import json
-# import logging
-# log = logging.getLogger("hvz.controllers")
+#!/usr/bin/env python
+#
+#   controllers.py
+#   HvZ
+#
 
-class Root(controllers.RootController):
+import logging
+
+import cherrypy
+import turbogears
+from turbogears import expose, url, identity, redirect
+
+from hvz import model #, json
+
+__author__ = 'Ross Light'
+__date__ = 'March 30, 2008'
+__all__ = ['log',
+           'Root',]
+
+log = logging.getLogger("hvz.controllers")
+
+class Root(turbogears.controllers.RootController):
     @expose(template="hvz.templates.welcome")
     # @identity.require(identity.in_group("admin"))
     def index(self):
@@ -15,17 +27,14 @@ class Root(controllers.RootController):
         # log.debug("Happy TurboGears Controller Responding For Duty")
         flash("Your application is now running")
         return dict(now=time.ctime())
-
+    
     @expose(template="hvz.templates.login")
     def login(self, forward_url=None, previous_url=None, *args, **kw):
-
         if not identity.current.anonymous and identity.was_login_attempted() \
                 and not identity.get_identity_errors():
-            raise redirect(tg.url(forward_url or previous_url or '/', kw))
-
+            raise redirect(url(forward_url or previous_url or '/', kw))
         forward_url = None
-        previous_url = request.path
-
+        previous_url = cherrypy.request.path
         if identity.was_login_attempted():
             msg = _("The credentials you supplied were not correct or "
                    "did not grant access to this resource.")
@@ -34,13 +43,16 @@ class Root(controllers.RootController):
                    "this resource.")
         else:
             msg = _("Please log in.")
-            forward_url = request.headers.get("Referer", "/")
-
-        response.status = 403
-        return dict(message=msg, previous_url=previous_url, logging_in=True,
-            original_parameters=request.params, forward_url=forward_url)
-
+            forward_url = cherrypy.request.headers.get("Referer", "/")
+        cherrypy.response.status = 403
+        return dict(message=msg,
+                    previous_url=previous_url,
+                    logging_in=True,
+                    original_parameters=cherrypy.request.params,
+                    forward_url=forward_url,)
+    
     @expose()
     def logout(self):
         identity.current.logout()
         raise redirect("/")
+
