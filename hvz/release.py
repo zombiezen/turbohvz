9c181e2629f020651f953a6455dd08c3ee0fca9d
Ross Light <ross@zombiezen.com>
2008-05-17 02:07:36 +0000

    Bumped version number to 0.3.0

--- hvz/release.py
+++ hvz/release.py
@@ -21,7 +21,7 @@
 
 """Release information about HvZ"""
 
-version = "0.3.0rc2"
+version = "0.3.0"
 
 description = "A web application for the game Humans vs. Zombies"
 long_description = "Humans vs. Zombies, created by Brad Sappington and " \
